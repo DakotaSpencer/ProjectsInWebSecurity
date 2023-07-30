77e47b4f7999fb151195a4112116792b0e7615c1
DakotaSpencer <61034586+DakotaSpencer@users.noreply.github.com>
2023-07-29 21:25:49 -0600

    you can uh... see the stuff now. And select every answer...

--- Time4Trivia-Start/routes/game.js
+++ Time4Trivia-Start/routes/game.js
@@ -3,10 +3,11 @@ const { getQuestions } = require('../controllers/gameController');
 const router = express.Router();
 
 router.get('/play', async function(req, res, next) {
+  let index=0;
   // TODO: Implement Game
   let questions = await getQuestions()
   
-  res.render('play', {user: req.session.user, questions});
+  res.render('play', {user: req.session.user, questionArray: questions, index: index});
 });
 
 module.exports = router;
